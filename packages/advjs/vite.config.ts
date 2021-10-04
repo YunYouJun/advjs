f011872188d1deb77a70037d72908c52ef78a688
YunYouJun <me@yunyoujun.cn>
2021-10-05 02:29:17 +0800

    :bento: add pwa 192 512 size icons

--- packages/advjs/vite.config.ts
+++ packages/advjs/vite.config.ts
@@ -117,17 +117,17 @@ export default defineConfig({
         theme_color: '#000',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/icons/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/icons/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/icons/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
