8cfb8c6e4d16fe419b600e0bb753b3021a62f9d2
YunYouJun <me@yunyoujun.cn>
2021-11-23 23:57:11 +0800

    refactor: remove markdown-it-adv package

--- packages/markdown-it-adv/README.md
+++
@@ -1,3 +0,0 @@
-# markdown-it-advjs
-
-基于 [markdown-it](https://github.com/markdown-it/markdown-it) 实现的 AdvScript 语法解析。
--- packages/shared/config/vite.ts
+++ packages/shared/config/vite.ts
@@ -6,5 +6,4 @@ export const commonAlias: AliasOptions = {
   '@advjs/editor/': `${path.resolve(__dirname, '../', '../editor/src')}/`,
   '@advjs/parser/': `${path.resolve(__dirname, '../', '../parser/src')}/`,
   '@advjs/shared/': `${path.resolve(__dirname, '../', '../shared/src')}/`,
-  'markdown-it-adv': `${path.resolve(__dirname, '../', '../markdown-it-adv/src')}/`,
 }
